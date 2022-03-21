a6f492e8af49f1e709ae19aa2e19961ce5ccdfd5
shlomo <shlomo.gevim@gmail.com>
2022-03-21 11:41:00 +0200

    21/3/2022    after addind time stap to comment

--- app/src/main/java/com/sg/pager10/model/Comment.kt
+++ app/src/main/java/com/sg/pager10/model/Comment.kt
@@ -1,10 +1,13 @@
 package com.sg.pager10.model
 
+import java.sql.Timestamp
+
 data class Comment(
     val commntId:String="",
     val postId:String="",
     val text:String="",
     val userName:String="",
     val userId:String="",
+    val timestamp: Timestamp
 
 )
